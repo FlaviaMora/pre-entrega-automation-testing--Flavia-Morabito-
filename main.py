09dfa08af8dbe264523afe1eab328d4352f8d9f3
Flavia <flaviamora.v@gmail.com>
2026-05-06 17:45:26 -0300

    Probando Selenium

---
+++ main.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+
+driver= webdriver.Chrome()
+'''
+driver.get("https://www.google.com")
+print("Probando si el entorno y Selenium funcionan...")
+driver.quit()
+'''
+
