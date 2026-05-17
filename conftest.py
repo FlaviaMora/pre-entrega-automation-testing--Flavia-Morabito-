ffe6300a83bc49350fe9f43552e5b42c313b08a1
Flavia <flaviamora.v@gmail.com>
2026-05-17 17:33:30 -0300

    agrego carpeta data con usuarios de prueba, fixture en conftest.py, logica en login_page.py y test_login.py

--- conftest.py
+++ conftest.py
@@ -1,8 +1,21 @@
-import pytest 
-from utils.helpers import get_driver
+import pytest
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 
-@pytest.fixture
+
+@pytest.fixture(scope="function")
+
 def driver():
-    driver=get_driver()
+#Fixture que proporciona un WebDriver configurado
+    chrome_options = Options()
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    service = Service()
+    driver = webdriver.Chrome(service=service, options=chrome_options)
+    driver.maximize_window()
+    driver.implicitly_wait(5)
     yield driver
+    time.sleep(1) # Para ver el resultado final
     driver.quit()
---
+++ test/test_login.py
@@ -0,0 +1,24 @@
+import pytest
+from pages.login_page import LoginPage
+from data.users import USERS
+
+class TestLogin:
+
+    @pytest.mark.parametrize("usuario, clave", USERS)
+    def test_login_parametrizado(self, driver, usuario, clave):
+        """Prueba el login con todos los usuarios de users.py"""
+        login_page = LoginPage(driver)
+        login_page.abrir().login_completo(usuario, clave)
+        
+        if usuario == "locked_out_user":
+            #validar que aparezca el cartel rojo
+            assert login_page.esta_error_visible(), "El error no apareció para el usuario bloqueado"
+            
+            #validar que el mensaje contenga "locked out"
+            mensaje_actual = login_page.obtener_mensaje_error()
+            assert "locked out" in mensaje_actual, f"Mensaje incorrecto. Se obtuvo: '{mensaje_actual}'"
+            
+        else:
+            # El resto de los usuarios deberían entrar al inventario
+            assert "inventory.html" in driver.current_url, f"El usuario {usuario} no fue redirigido. URL: {driver.current_url}"
+            assert driver.title == "Swag Labs", f"El título no es correcto para el usuario {usuario}"
